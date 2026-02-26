aee38c30d4a0679b2a97af711e2dfa1165082dea
juanjo <jjsanchezmedina@gmail.com>
2026-02-26 12:39:04 -0500

    feat: first commit

---
+++ src/content.config.ts
@@ -0,0 +1,35 @@
+import { glob } from 'astro/loaders';
+import { defineCollection, z } from 'astro:content';
+
+const blog = defineCollection({
+	// Load Markdown and MDX files in the `src/content/blog/` directory.
+	loader: glob({ base: './src/content/blog', pattern: '**/*.{md,mdx}' }),
+	// Type-check frontmatter using a schema
+	schema: z.object({
+		title: z.string(),
+		description: z.string(),
+		// Transform string to Date object
+		pubDate: z.coerce.date(),
+		updatedDate: z.coerce.date().optional(),
+		heroImage: z.string().optional(),
+	}),
+});
+
+const projects = defineCollection({
+	// Load Markdown and MDX files in the `src/content/projects/` directory.
+	loader: glob({ base: './src/content/projects', pattern: '**/*.{md,mdx}' }),
+	// Type-check frontmatter using a schema
+	schema: z.object({
+		title: z.string(),
+		description: z.string(),
+		url: z.string(),
+		image: z.string().optional(),
+		labels: z.array(z.string()).optional(),
+		hidden: z.boolean().optional(),
+		packageLabel: z.string().optional(),
+		packageUrl: z.string().optional(),
+		sourceUrl: z.string().optional(),
+	}),
+});
+
+export const collections = { blog, projects };
